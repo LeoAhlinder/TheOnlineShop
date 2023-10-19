15deb89fe41da76c29f64ada81246492249ed531
Leo Ahlinder <ahlinder.leo@icloud.com>
2023-10-19 11:39:42 +0200

    Header design and for mobile

--- shop/src/Header/Header.jsx
+++ shop/src/Header/Header.jsx
@@ -1,9 +1,51 @@
-function Header() {
-  return (
-    <div>
-      <h2>test</h2>
-    </div>
-  );
+import React, { useState } from 'react';
+import "./headerStyle.css"
+
+const Header = () => {
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+      setIsMenuOpen(prevState => !prevState);
+    };
+  
+    const closeMenu = () => {
+      setIsMenuOpen(false);
+    };
+
+
+
+    return (
+        <header className='layers'>
+            <div className="container"> 
+                <nav className="navbar" id='nav-menu'>
+                    <a href="/" className="nav-branding">THS</a>
+                    <ul className={`nav-menu ${isMenuOpen ? 'active' : ''}`}>
+                        <li className="navItem">
+                            <a className='navLink' onClick={closeMenu}>test</a>
+                        </li>
+                        <li className="navItem">
+                            <a className="navLink" onClick={closeMenu}>test2</a>
+                        </li>
+                        <li className="navItem">
+                            <a className="navLink" onClick={closeMenu}>test3</a>
+                        </li>
+                        <li className="navItem">
+                            <a className='navLink' >
+                                test4
+                            </a>
+                        </li>
+                    </ul>
+                    <div className={`hamburger ${isMenuOpen ? 'active' : ''}`} onClick={toggleMenu} id='hamburger'>
+                        <span className="bar"></span>
+                        <span className="bar"></span>
+                        <span className="bar"></span>
+                    </div>
+                </nav>
+            </div>
+        </header>
+      );
+      
 }
 
-export default Header;
+export default Header
